a92f338c604dba943aad01fc5cd33a10a54937ec
Trunin Anton <oknoaa01@mail.ru>
2019-04-23 21:25:06 +0300

    ard.ino

--- ard/ard.ino
+++ ard/ard.ino
@@ -1,22 +1,30 @@
 void func() {
-  randomSeed(analogRead(A0));
-  delay(random(1000, 4000));
-  Serial.println("Done");
+	randomSeed(analogRead(A0));
+	delay(random(100, 400));
+	Serial.println("Done");
+}
+void func2() {
+	randomSeed(analogRead(A0));
+	int x =(random(100, 400);
+	Serial.println(x);
 }
 void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  //func();
+	Serial.begin(9600);
+	pinMode(LED_BUILTIN, OUTPUT);
+	//func();
 }
 void loop() {
-  while (!Serial.available());
-  char x = Serial.read();
-  switch (x) {
-    case ('M'):
-      func();
-      break;
-    //default:
-      //Serial.println(x);
-      // ololo ololo tralala
-  }
+	while (!Serial.available());
+	char x = Serial.read();
+	switch (x) {
+	case ('M'):
+		func();
+		break;
+	case ('X'):
+		finc2();
+		break;
+	default:
+		Serial.println(x);
+		break;
+	}
 }
