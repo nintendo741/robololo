ac9425d713bdd5acf78aeee6b5514f9f305ae771
Imbalanceone <Imbalance.one2277@gmail.com>
2019-04-24 11:24:43 +0300

    fix some bugs

--- Socked_Recv.py
+++ Socked_Recv.py
@@ -5,6 +5,7 @@ def PrepareData (data,FrameId):
     for i in range(len(data[0])):
         for j in range(6):
             print(data[j][i],end=" ")
+        print()
         DB.JsonWrite(FrameId,data[0][i],data[1][i],data[2][i],data[3][i],data[4][i],data[5][i])
 DB.DBConnect() #убрать потом это говно
 FrameId=0
