51794008bdaa7f25024db5c9db80eb273a1bd28b
Imbalanceone <Imbalance.one2277@gmail.com>
2019-04-24 12:28:51 +0300

    socked now worked

--- Socked_Recv.py
+++ Socked_Recv.py
@@ -3,9 +3,7 @@ import json
 from Utils import DBConnector as DB
 def PrepareData (data,FrameId):
     for i in range(len(data[0])):
-        for j in range(6):
-            print(data[j][i],end=" ")
-        print()
+
         DB.JsonWrite(FrameId,data[0][i],data[1][i],data[2][i],data[3][i],data[4][i],data[5][i])
 DB.DBConnect() #убрать потом это говно
 FrameId=0
@@ -22,15 +20,20 @@ try:
             data = conn.recv(2048)
             if not data:
                 break
-            print(data)
-            FrameId+=1
+
             data=data.decode()
-            if data!='Nothing':
-                data=json.loads(data)
-                print(data)
-                PrepareData(data,FrameId)
+            data=data.split("|")
+
+
+            if data[0]!='Nothing':
+                for i in range(len(data)-1):
+                    frameData=json.loads(data[i])
+                    FrameId+=1
+                    PrepareData(frameData,FrameId)
             else:
+                FrameId+=1
                 DB.JsonWrite(FrameId,0,0,0,0,0,0)
+
 except KeyboardInterrupt:
     conn.close()
 
