db649e817b46b98770caab1e00506a600065d7a4
Imbalanceone <Imbalance.one2277@gmail.com>
2019-04-25 09:05:53 +0300

    hren

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -9,18 +9,20 @@ MovingArduinoPath = 0
 #порты TTY, где Ардуино
 #ACM на все порты
 
-while(True):
+
+
+
+def ChekStatus ():
 	Action = QU.QueryMinId('queryArduino')
 	if (QU.QueryStatusCheck('queryArduino', Action) == 1):
 		QU.QueryStatusChange('queryArduino', Action, 2)
-		StartPhase(Action)
+		#StartPhase(Action)
 	elif(QU.QueryStatusCheck('queryArduino', Action) == 3):
 		QU.QueryToLog('queryArduino', Action)
 		QU.QueryDelete('queryArduino', Action)
 	time.sleep(0.1)
 	if (False):
 		QU.QueryStatusChange('queryArduino', Action, 3)
-
 
 # из Бд брать очередь
 #     если статус 1, изменить на 2(в процессе)
